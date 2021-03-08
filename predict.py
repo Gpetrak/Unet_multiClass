f6229364e285ddfe47fac4a41ca7f839bb65d381
gpetrak <gkpetrak@gmail.com>
2021-03-08 10:45:55 +0200

    fixing a syntax error

--- predict.py
+++ predict.py
@@ -46,6 +46,6 @@ def display(display_list,title=['Input Image', 'True Mask', 'Predicted Mask']):
     for i in range(len(display_list)):
         plt.subplot(1, len(display_list), i+1)
         plt.title(title[i])
-         plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]),cmap='magma')
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]),cmap='magma')
         plt.axis('off')
     plt.show()
